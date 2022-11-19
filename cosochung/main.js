c1cc2fb1dd3439a653cd28067a31eebfbf9bfd44
trungsondev <96918418+trungsondev@users.noreply.github.com>
2022-11-19 22:24:07 +0700

    lan1

---
+++ cosochung/main.js
@@ -0,0 +1,485 @@
+var dapan = {
+    1: "c",
+    2: "c",
+    3: "a",
+    4: "a",
+    5: "a",
+    6: "c",
+    7: "b",
+    8: "b",
+    9: "c",
+    10: "a",
+    11: "a",
+    12: "c",
+    13: "b",
+    14: "a",
+    15: "a",
+    16: "c",
+    17: "b",
+    18: "d",
+    19: "b",
+    20: "a",
+    21: "c",
+    22: "a",
+    23: "a",
+    24: "d" /*a*/ ,
+    25: "a" /*a*/ ,
+    26: "c" /*a*/ ,
+    27: "b" /*a*/ ,
+    28: "b" /*a*/ ,
+    29: "a" /*a*/ ,
+    30: "c" /*a*/ ,
+    31: "b" /*a*/ ,
+    32: "d" /*a*/ ,
+    33: "b" /*a*/ ,
+    34: "c" /*a*/ ,
+    35: "c" /*a*/ ,
+    36: "a" /*a*/ ,
+    37: "d" /*a*/ ,
+    38: "a" /*a*/ ,
+    39: "b" /*a*/ ,
+    40: "d" /*a*/ ,
+    41: "b" /*a*/ ,
+    42: "d" /*a*/ ,
+    43: "d" /*a*/ ,
+    44: "b" /*a*/ ,
+    45: "c" /*a*/ ,
+    46: "c" /*a*/ ,
+    47: "a" /*a*/ ,
+    48: "a" /*a*/ ,
+    49: "b" /*a*/ ,
+    50: "d" /*a*/ ,
+    51: "a" /*a*/ ,
+    52: "b" /*a*/ ,
+    53: "b" /*a*/ ,
+    54: "a",
+    55: "b",
+    56: "d",
+    57: "b",
+    58: "a" /*a*/ ,
+    59: "c" /*a*/ ,
+    60: "d" /*a*/ ,
+    61: "a" /*a*/ ,
+    62: "b" /*a*/ ,
+    63: "b",
+    64: "a",
+    65: "a",
+    66: "c" /*a*/ ,
+    67: "a" /*a*/ ,
+    68: "d",
+    69: "c",
+    70: "a" /*a*/ ,
+    71: "a" /*a*/ ,
+    72: "c",
+    73: "a" /*a*/ ,
+    74: "a" /*a*/ ,
+    75: "a" /*a*/ ,
+    76: "c",
+    77: "a",
+    78: "b",
+    79: "a",
+    80: "c",
+    81: "b",
+    82: "b",
+    83: "a",
+    84: "b",
+    85: "a",
+    86: "a",
+    87: "d",
+    88: "d",
+    89: "d",
+    90: "d",
+    91: "a",
+    92: "b",
+    93: "d",
+    95: "b",
+    96: "c",
+    97: "d",
+    98: "d",
+    99: "a",
+    100: "c",
+    101: "b",
+    102: "a",
+    103: "b",
+    104: "a",
+    105: "c",
+    106: "d",
+    107: "b",
+    108: "a",
+    109: "b",
+    110: "d",
+    111:"f",
+    112: "d",
+    113: "b",
+    114: "c",//a
+    115: "d",//a
+    116: "b",//a
+    117: "c",//a
+    118: "a",//a
+    119: "d",//a
+    120: "b",//a
+    121: "b",//a
+    122: "c",//a
+    123: "a",//a
+    124: "a",//a
+    125: "b",//a
+    126: "c",//a
+    127: "c",//a
+    128: "b",//a
+    129: "a",//a
+    130: "d",//a
+    131: "b",//a
+    132: "c",//a
+    133: "b",//a
+    134: "c",//a
+    135: "a",//a
+    136:"f",
+    137: "a",
+    138: "a",
+    139: "a",
+    140: "a",
+    141: "f",
+    142: "f",
+    143: "d",
+    144: "d",//a
+    145: "c",//a
+    146: "a",//a
+    147: "a",//a
+    148: "d",//a
+    149: "d",
+    150: "c",//a
+    151: "a",//a
+    152: "a",//a
+    153: "c",
+    154: "b",
+    155: "d",
+    156: "a",
+    157: "c",
+    158: "b",
+    159: "f",
+    160: "d",
+    161: "c",
+    162: "a",
+    163: "b",
+    164: "b",
+    165: "b",
+    166: "c",
+    167: "a",
+    168: "c",
+    169: "b",
+    170: "b",
+    171: "c",
+    172: "d",
+    173: "d",
+    174: "a",
+    175: "b",
+    176: "b",
+    177: "b",
+    178: "a",
+    179: "a",
+    180: "a",
+    181: "c",
+    182: "b",
+    183: "b",
+    184: "a",
+    185: "c",
+    186: "a",//a
+    187: "d",//a
+    188: "c",//a
+    189: "d",//a
+    190: "b",//a
+    191: "c",//a
+    192: "a",//a
+    193: "d",//a
+    194: "a",//a
+    195: "a",//a
+    196: "a",//a
+    197: "c",
+    198: "c",
+    199: "c",
+    200: "d",
+    201: "a",
+    202: "a",
+    203: "b",
+    204: "a",
+    205: "b",
+    206: "a",
+    207: "c",
+    208: "d",
+    209: "a",
+    210: "b",
+    211: "c",
+    212: "c",
+    213: "d",
+    214: "c",
+    215: "c",
+    216: "a",//a
+    217: "a",//a
+    218: "a",//a
+    219: "a",//a
+    220: "a",//a
+    221: "a",//a
+    222: "a",//a
+    223: "a",//a
+    // 224: "c",
+    225: "a",//a
+    226: "a",//a
+    227: "a",//a
+    228: "a",//a
+    229: "a",//a
+    230: "a",//a
+    231: "a",//a
+    232: "a",//a
+    233: "c",
+    234: "b",
+    235: "a",//a
+    236: "d",
+    237: "a",//a
+    238: "a",//a
+    239: "a",//a
+    240: "d",
+    241: "c",
+    242: "a",//a
+    243: "a",//a
+    244: "c",
+    245: "c",
+    246: "a",//a
+    247: "a",//a
+    248: "a",//a
+    249: "a",//a
+    250: "a",//a
+    251: "c",
+    252: "c",
+    253: "c",
+    254: "d",
+    255: "c",
+    256: "c",
+    257: "d",
+    258: "d",
+    259: "a",//a
+    260: "a",//a
+    261: "c",
+    262: "a",//a
+    263: "a",//a
+    264: "c",
+    265: "b",
+    266: "b",
+    267: "a",//a
+    268: "a",//a
+    269: "d",
+    270: "a",//a
+    271: "a",//a
+    272: "c",
+    273: "c",
+    274: "a",//a
+    275: "a",//a
+    276: "a",//a
+    277: "a",//a
+    278: "a",//a
+    279: "d",
+    280: "a",//a
+    281: "a",//a
+    282: "b",
+    283: "a",//a
+    284: "a",//a
+    285: "c",
+    286: "a",//a
+    287: "a",//a
+    288: "a",//a
+    289: "a",//a
+    290: "a",//a
+    291: "a",//a
+    292: "a",//a
+    293: "a",//a
+    294: "a",//a
+    295: "a",//a
+    296: "a",//a
+    297: "c",
+    298: "c",
+    299: "c",
+    300: "b",
+    301: "b",
+    302: "d",
+    303: "d",
+    304: "a",//a
+    // 305: "a",//a
+    306: "a",//a
+    307: "c",
+    308: "a",//a
+    309: "a",//a
+    310: "a",//a
+    311: "a",//a
+    312: "b",
+    313: "a",//a
+    314: "a",//a
+    315: "b",
+    316: "a",//a
+    317: "a",//a
+    318: "a",//a
+    319: "a",//a
+    320: "a",//a
+    321: "a",//a
+    322: "d",
+    323: "a",//a
+    324: "a",//a
+    325: "a",//a
+    326: "a",//a
+    327: "a",//a
+    328: "a",//a
+    329: "a",//a
+    330: "a",//a
+    331: "a",//a
+    332: "a",//a
+    333: "a",//a
+    334: "a",//a
+    335: "a",//a
+    336: "a",//a
+    337: "a",//a
+    338: "a",//a
+    339: "a",//a
+    340: "a",//a
+    341: "a",//a
+    342: "a",//a
+    343: "a",//a
+    344: "a",//a
+    345: "a",//a
+    // 346: "a",//a
+    347: "a",//a
+    348: "a",//a
+    349: "a",//a
+    350: "a",//a
+    351: "a",//a
+    352: "a",//a
+    353: "a",//a
+    354: "a",//a
+    355: "a",//a
+    356: "a",//a
+    357: "a",//a
+    358: "a",//a
+    359: "a",//a
+    360: "a",//a
+    361: "a",//a
+    362: "a",//a
+    363: "a",//a
+    364: "a",//a
+    365: "a",//a
+    366: "a",//a
+    367: "a",//a
+    368: "a",//a
+    369: "a",//a
+    370: "a",//a
+    372: "a",//a
+    373: "a",//a
+    374: "a",//a
+    375: "a",//a
+    376: "a",//a
+    377: "a",//a
+    378: "a",//a
+    379: "a",//a
+    380: "a",//a
+    381: "a",//a
+    382: "a",//a
+    383: "a",//a
+    384: "a",//a
+    385: "a",//a
+    386: "a",//a
+    387: "a",//a
+    388: "a",//a
+    389: "a",//a
+    390: "a",//a
+
+}
+var xx = 1;
+
+function readTextFile() {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "cau.txt", true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4) {
+            var json = JSON.stringify(rawFile.responseText);
+            var de = rawFile.responseText.split("\n")
+            thihanh(de);
+        }
+    }
+    rawFile.send();
+}
+//305 346 371 94 111 136 141 142 159
+var i = 1;
+
+function cong() {
+    if (i < 391) {
+        i += 1;
+        console.log(i)
+        readTextFile()
+    }
+
+    document.getElementById("trungson").style.backgroundColor = "#555555";
+}
+
+function tru() {
+    if (i > 1) {
+        i -= 1;
+        readTextFile()
+    }
+    document.getElementById("trungson").style.backgroundColor = "#555555";
+}
+var right=0;
+
+function nhanham(socau, traloi) {
+    console.log(socau)
+    console.log(traloi)
+    if (dapan[socau] == traloi) {
+        document.getElementById("trungson").style.backgroundColor = "green";
+        right++;
+    } else {
+        document.getElementById("trungson").style.backgroundColor = "red";
+    }
+    var ht=`${right}`;
+    $("#diem").html(ht);
+}
+
+function thihanh(de) {
+    var tt = (i - 1) * 5;
+    console.log(tt)
+        // const mang= de.filter((a) => a);
+    var ht = `
+    <div class="container mt-sm-1 my-1" id="trungson">
+        <div class="question ml-sm-5 pl-sm-5 pt-2">
+          <div class="py-2 h5"><b>${de[tt]}</b></div>
+          <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options"> 
+              <label class="options">${de[tt+1]} <input type="radio" name="radio" onclick="nhanham(${i},'a')"> <span class="checkmark"></span> </label> 
+              <label class="options">${de[tt+2]} <input type="radio" name="radio" onclick="nhanham(${i},'b')"> <span class="checkmark"></span> </label> 
+              <label class="options">${de[tt+3]} <input type="radio" name="radio" onclick="nhanham(${i},'c')"> <span class="checkmark"></span> </label> 
+              <label class="options">${de[tt+4]} <input type="radio" name="radio" onclick="nhanham(${i},'d')"> <span class="checkmark"></span> </label> 
+          </div>
+        </div>
+        <div class="d-flex align-items-center pt-3">
+            <div id="prev"> <button class="btn btn-primary" onclick="tru()">Trước</button> </div>
+            <div class="ml-auto mr-sm-5"> <button class="btn btn-success" onclick="cong()">tiếp tục</button> </div>
+        </div>
+    </div>
+    `
+
+    document.getElementById("son").innerHTML = ht;
+
+}
+readTextFile();
+
+// for (let i = 0; i < de.length; index++) {
+
+// }
+function dropxuat() {
+    var ht = ``;
+    for (let index = 0; index < 100; index += 10) {
+        ht += `
+        <option value=${index+1}><strong>Từ câu ${index+1} </strong></option>
+      `
+    }
+    document.getElementById("exampleFormControlSelect1").innerHTML = ht
+}
+dropxuat()
+document.getElementById("exampleFormControlSelect1").addEventListener("change", function() {
+    i = parseInt(this.value);
+    console.log(i)
+    readTextFile();
+})
